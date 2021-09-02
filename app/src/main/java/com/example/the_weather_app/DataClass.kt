edc7668a6ee3e99f738ac16f159fa0b213e69ec4
AizakoDev <Aizako96@gmail.com>
2021-09-03 00:45:17 +0300

    второй коммит

---
+++ app/src/main/java/com/example/the_weather_app/DataClass.kt
@@ -0,0 +1,9 @@
+package com.example.the_weather_app
+
+data class DataClass(
+    var cityAndCountry: String,
+    var currentTemperature: Int,
+    var currentWeatherCondition: Int,
+    var weatherInformation: String,
+    var humidity: Int
+)
--- app/src/main/java/com/example/the_weather_app/MainActivity.kt
+++ app/src/main/java/com/example/the_weather_app/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.the_weather_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button1 = findViewById<Button>(R.id.button1)
+        button1.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(p0: View?) {
+                TODO("Not yet implemented")
+            }
+        })
     }
+
+    fun Click(view: View) {
+    }
+
 }
