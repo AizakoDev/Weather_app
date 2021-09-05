2f8758148064aac67a123805926296b4b20bec1f
AizakoDev <Aizako96@gmail.com>
2021-09-06 01:36:42 +0300

    создаем архитектуру

--- app/src/main/java/com/example/the_weather_app/view/MainActivity.kt
+++ app/src/main/java/com/example/the_weather_app/view/MainActivity.kt
@@ -1,14 +1,19 @@
-package com.example.the_weather_app
+package com.example.the_weather_app.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import com.example.the_weather_app.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null)
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container,MainFragment,newInstance()).commit()
+
 
         val button1 = findViewById<Button>(R.id.button1)
         button1.setOnClickListener(object : View.OnClickListener{
@@ -16,9 +21,13 @@ class MainActivity : AppCompatActivity() {
                 TODO("Not yet implemented")
             }
         })
+
+
     }
 
     fun Click(view: View) {
     }
+
+
 
 }
---
+++ app/src/main/java/com/example/the_weather_app/view/MainFragment.kt
@@ -0,0 +1,26 @@
+package com.example.the_weather_app.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class MainFragment: Fragment() {
+
+    /*1*/ companion object{
+        fun newInstance(): Companion {
+            return MainFragment
+        }
+    }
+    /*2*/   //или просто: fun newInstance = MainFragment
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+}
